81e2e219bcdf03c62699e1e152000ca015bae3a5
YoungDuck1592 <tjdudejr1919@gmail.com>
2023-07-24 19:37:32 +0900

    "은행점검 시 결제 오류 구문 작성 / 서영덕"

--- Kiosk/main.swift
+++ Kiosk/main.swift
@@ -8,3 +8,30 @@
 import Foundation
 
 print("Hello, World!")
+
+
+// 한국 시간 (요것이 main에 적어야만 작동이 되는 것 같습니다. payment 파일에 작성하니 상위 권한이 있는 파일에 적으라고 오류가 나네용)
+
+let now = Date()
+let koreaTime = DateFormatter()
+koreaTime.dateFormat = "HH:mm:ss"
+koreaTime.timeZone = TimeZone(identifier: "Asia/Seoul")
+let nowTime = koreaTime.string(from: now)
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
