d78d74586808847eda497afde1a74c4ee3676f98
Taeshiki <durudurutta@gmail.com>
2023-07-24 10:11:13 +0900

    키오스크 프로젝트

---
+++ kiosk/kiosk/kioskApp.swift
@@ -0,0 +1,17 @@
+//
+//  kioskApp.swift
+//  kiosk
+//
+//  Created by 요시킴 on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct kioskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
