898c34614a54e115ca2bd8822d86d593c604ebe6
angwoo0503 <angwoo0503@gmail.com>
2023-07-27 16:34:23 +0900

    while문 적용 AutoMaticOutput 분리 / 박상우

--- Kiosk/AutoMaticOutput.swift
+++ Kiosk/AutoMaticOutput.swift
@@ -1,24 +1,38 @@
 import Foundation
 
+// waitingList_now 고객대기수이며 주문 성공 시 +1 추가 디폴트 0으로 설정
+var waitingListNow : Int = 0
+// roopTime_Want 원하는 루프타임 설정 디폴트 5로 설정
+var roopTimeWant : Int = 10
+var roopCount : Int = 100
+
 class AutoMaticOutput {
-    var waitingList : [Int] = Array(1...10)
-    var roopTime : Int
-    var timer: Timer?
-    
-    init(roopTime: Int) {
-        self.roopTime = roopTime
+  var waitingList : Int
+  var roopTime : Int
+  var timer: Timer?
+  var count = 0
+
+  init(roopTime: Int, waitingList: Int) {
+    self.roopTime = roopTime
+    self.waitingList = waitingList
+  }
+
+  func start() {
+    let queue = DispatchQueue(label: "com.example.myQueue")
+    queue.async {
+      self.timer = Timer(timeInterval: TimeInterval(self.roopTime), repeats: true) { [self] _ in
+        print("현재 대기 중인 주문은 \(Int(waitingList))건 입니다.")
+        self.count += 1
+        if self.count >= roopCount {
+            self.end()
+        }
+      }
+      RunLoop.current.add(self.timer!, forMode: .default)
+      RunLoop.current.run()
     }
-    
-    func start() {
-        
-        let queue = DispatchQueue(label: "com.example.myQueue")
-        
-        queue.async {
-            let timer = Timer(timeInterval: TimeInterval(self.roopTime), repeats: true) { [self] _ in
-        print("현재 대기 중인 주문은 \(Int(waitingList.randomElement() ?? 0))건 입니다.")
-        }
-        RunLoop.current.add(timer, forMode: .default)
-        RunLoop.current.run()
-        }
+  }
+    func end() {
+      timer?.invalidate()
+      timer = nil
     }
 }
